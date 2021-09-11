c6d287c756e59de9469349697b352fc6f649efe0
Felipe <felipe.faria@livetouch.com.br>
2021-09-11 10:50:41 -0300

    Adicioando template base e comandos no arquivo Readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Projeto-InFatec
+
+```bash
+git clone #
+cd Projeto-InFatec
+```
+Para Windows:
+```bash
+python3 -m venv env
+.\env\Scripts\activate
+```
+
+Para Linux:
+```bash
+python3 -m venv env
+source env/bin/activate
+```
+
+Instalar dependencias:
+```bash
+pip3 install -r requirements.txt
+```
+
+Executar aplicação:
+```bash
+python3 wsgi.py
+```
+git clone #
+```
--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        # fazer aqui validação de login
+        return redirect(url_for('inicio'))
+    else:
+        return render_template('login.html')
 
 @app.route('/redefinir')
 def redefinir():
     return render_template('redefinir_senha.html')
+
+@app.route('/')
+def inicio():
+    return render_template('base.html')
+
+@app.route('/post')
+def post():
+    return render_template('posts.html')
