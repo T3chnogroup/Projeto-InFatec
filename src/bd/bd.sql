b5a9f2f6f5ab14612ffdaca8e12b3f4e8bb82d44
Lucas Braz <lucasbrzdias@gmail.com>
2021-10-06 12:17:48 -0300

    alteraçao no banco de dados

--- src/bd/bd.sql
+++ src/bd/bd.sql
@@ -10,29 +10,15 @@ CREATE TABLE IF NOT EXISTS usuario (
 	cpf CHAR(11) NOT NULL
 ) ENGINE=INNODB;
 
-CREATE TABLE IF NOT EXISTS usuario_canal (
-	fk_usuario INT,
-	fk_canal INT,
-	PRIMARY KEY (fk_usuario, fk_canal),
-	FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario),
-	FOREIGN KEY (fk_canal) REFERENCES canal (id_canal)
-);
-
 CREATE TABLE IF NOT EXISTS canal (
 	id_canal INT AUTO_INCREMENT PRIMARY KEY,
 	nome VARCHAR(30) NOT NULL,
+	grupo INT,
+	semestre INT,
+	curso INT,
 	fk_usuario INT,
 	FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario)
 ) ENGINE=INNODB;
-
-CREATE TABLE IF NOT EXISTS visualizado_por (
-	fk_usuario INT,
-	fk_post INT,
-	data_visualizacao DATE,
-	PRIMARY KEY (fk_usuario, fk_post),
-	FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario),
-	FOREIGN KEY (fk_post) REFERENCES post (id_post)
-);
 
 CREATE TABLE IF NOT EXISTS post (
 	id_post INT AUTO_INCREMENT PRIMARY KEY,
@@ -51,6 +37,23 @@ CREATE TABLE IF NOT EXISTS anexo (
 	fk_post INT,
 	FOREIGN KEY (fk_post) REFERENCES post (id_post)
 ) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS visualizado_por (
+	fk_usuario INT,
+	fk_post INT,
+	data_visualizacao DATE,
+	PRIMARY KEY (fk_usuario, fk_post),
+	FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario),
+	FOREIGN KEY (fk_post) REFERENCES post (id_post)
+);
+
+CREATE TABLE IF NOT EXISTS usuario_canal (
+	fk_usuario INT,
+	fk_canal INT,
+	PRIMARY KEY (fk_usuario, fk_canal),
+	FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario),
+	FOREIGN KEY (fk_canal) REFERENCES canal (id_canal)
+);
 
 -- Inserção tabela usuario
 INSERT INTO usuario VALUES (0, "Lucas", "lucasbrzdias@gmail.com", "123456", "33333333333")
