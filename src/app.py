6ac4b4b4ed11d6ed89211864b8d10417431c8a75
taissalomao <tais.salomao@fatec.sp.gov.br>
2021-10-08 14:13:35 -0300

    Adicionado variáveis de ambiente.

--- src/app.py
+++ src/app.py
@@ -1,16 +1,19 @@
+import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_mysqldb import MySQL
 from datetime import date
+from dotenv import load_dotenv
+load_dotenv(".env")
 
 app = Flask(__name__)
 
 # configuração Conexão com o Banco de Dados Mysql
-# app.config['MYSQL_Host'] = 'localhost'
-app.config['MYSQL_HOST'] = '0.0.0.0'
+app.config['MYSQL_Host'] = os.getenv("MYSQL_Host")
+#app.config['MYSQL_HOST'] = '0.0.0.0'
 app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'dsm!123456'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'infatec'
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+#app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 
 mysql = MySQL(app)
 
