0dd5188f7909037ca350ff0c72f2e79abbf15361
Lucas Braz <lucasbrzdias@gmail.com>
2021-11-20 16:57:39 -0300

    Revert "funçao para recuperar cookie em nova_senha"
    This reverts commit d8fb8b2a625cc21b157a514213f0f80b1002981f.

--- src/static/js/nova_senha.js
+++ src/static/js/nova_senha.js
@@ -1,9 +1,3 @@
-function get_cookie(name){
-  return document.cookie.split(';').some(c => {
-    return c.trim().startsWith(name + '=');
-  });
-}
-
 function delete_cookie( name, path, domain ) {
   if( get_cookie( name ) ) {
     document.cookie = name + "=" +
